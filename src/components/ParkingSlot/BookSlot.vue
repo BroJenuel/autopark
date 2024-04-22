30532d6dc3424a9b75d9d810c893a8d5175b804d
Paolo Vincent Julian <paolo.julian@accelo.com>
2024-04-22 21:18:28 +0800

    Refactor bookSlot function and add payment amount calculation

--- src/components/ParkingSlot/BookSlot.vue
+++ src/components/ParkingSlot/BookSlot.vue
@@ -1,14 +1,15 @@
 <script setup>
-import { ref } from "vue";
-import { supabaseClient } from "@/service/supabase/supabase";
-import { useToast } from "primevue/usetoast";
-import { Loading } from "notiflix";
 import Payment from "@/components/PaymentSimulation/Payment.vue";
+import { supabaseClient } from "@/service/supabase/supabase";
+import { Loading } from "notiflix";
+import { useToast } from "primevue/usetoast";
+import { ref } from "vue";
 
 const PaymentRef = ref(null);
 const toast = useToast();
 const showModal = ref(false);
 const showConfirmModal = ref(false);
+const hoursToOccupy = ref(null);
 const slotDetails = ref(null);
 const selectedPaymentMethod = ref(null);
 const paymentMethodData = ref(null);
@@ -18,22 +19,49 @@ function toggleModal(data) {
     showModal.value = !showModal.value;
 }
 
+/**
+ * Get rates from settings
+ *
+ * @returns {Promise<number>}
+ */
+async function getPaymentAmount() {
+    const { data, error } = await supabaseClient.from("settings").select().eq("id", 1);
+    if (error || data.length === 0) {
+        throw new Error(error);
+    }
+
+    if (hoursToOccupy.value === 2) {
+        return data[0].first_two_hours;
+    }
+
+    return data[0].first_two_hours + data[0].exceed_rate_per_hour;
+}
+
 async function bookSlot() {
     const parkingSlotId = slotDetails.value.id;
     const timeStarted = new Date();
 
     Loading.hourglass("booking slot...");
 
-    const { error } = await supabaseClient
-        .from("parking_slot_booking")
-        .insert([
-            {
-                parking_slot_id: parkingSlotId,
-                time_started: timeStarted,
-                payment_method: selectedPaymentMethod.value,
-                payment_method_details: paymentMethodData.value
-            }
-        ]);
+    /** @type {number} */
+    let payment_amount = 0;
+    /** @type {Date | null} */
+    let paid_at = null;
+    if (selectedPaymentMethod.value === "gcash" || selectedPaymentMethod.value === "maya") {
+        payment_amount = await getPaymentAmount();
+        paid_at = new Date();
+    }
+
+    const { error } = await supabaseClient.from("parking_slot_booking").insert([
+        {
+            parking_slot_id: parkingSlotId,
+            time_started: timeStarted,
+            payment_method: selectedPaymentMethod.value,
+            payment_method_details: paymentMethodData.value,
+            payment_amount,
+            paid_at,
+        },
+    ]);
 
     Loading.remove();
 
@@ -44,9 +72,9 @@ async function bookSlot() {
 
     toast.add({
         severity: "success",
-        summary: `Success Booked Slot - ${slotDetails.area}, ${slotDetails.street}`,
+        summary: `Success Booked Slot - ${slotDetails.value.area}, ${slotDetails.value.street}`,
         detail: "Slot booked",
-        life: 6000
+        life: 6000,
     });
 
     showModal.value = false;
@@ -56,50 +84,92 @@ async function bookSlot() {
     // redirect to my booking
 }
 
+const handle2Hours = () => {
+    console.log("2 hours");
+    showConfirmModal.value = true;
+    hoursToOccupy.value = 2;
+};
+
+const handle3Hours = () => {
+    console.log("3 hours");
+    showConfirmModal.value = true;
+    hoursToOccupy.value = 3;
+};
+
 const emit = defineEmits(["booked"]);
 
 defineExpose({
-    toggleModal
+    toggleModal,
 });
-
 </script>
 
 <template>
-    <Payment ref="PaymentRef" @pay="(paymentData) => {
-        paymentMethodData = paymentData;
-        bookSlot();
-    }" />
+    <Payment
+        ref="PaymentRef"
+        @pay="
+            (paymentData) => {
+                paymentMethodData = paymentData;
+                bookSlot();
+            }
+        "
+    />
     <Dialog v-model:visible="showModal" :style="{ width: '50rem', margin: '0 10px' }" header="Occupy Slot?" modal>
         <div v-if="slotDetails" class="mb-5">
             <div
-                v-for="(value, key) in Object.fromEntries(Object.entries(slotDetails).filter(([key, value]) => !['id','created_at'].includes(key)))">
-                <p>{{ key }}: <b>{{ value }}</b></p>
+                v-for="(value, key) in Object.fromEntries(
+                    Object.entries(slotDetails).filter(([key, value]) => !['id', 'created_at'].includes(key)),
+                )"
+            >
+                <p>
+                    {{ key }}: <b>{{ value }}</b>
+                </p>
             </div>
         </div>
         <div class="flex flex-column gap-3">
-            <Button label="Yes" @click="showConfirmModal = true" />
+            <Button label="Occupy for 2 hours" @click="handle2Hours" />
+            <Button label="Occupy for 3 hours" @click="handle3Hours" />
             <Button label="Close" severity="secondary" @click="showModal = false" />
         </div>
     </Dialog>
-    <Dialog v-model:visible="showConfirmModal" :style="{ width: '40rem', margin: '0 10px' }"
-            header="Choose Payment Method"
-            modal>
-
+    <Dialog
+        v-model:visible="showConfirmModal"
+        :style="{ width: '40rem', margin: '0 10px' }"
+        header="Choose Payment Method"
+        modal
+    >
         <div class="flex flex-column gap-3 mt-5">
             <p class="m-0">Occupy Easily With Online Pay</p>
-            <Button label="GCash" severity="info"
-                    @click="selectedPaymentMethod = 'gcash'; PaymentRef.toggleDialog('gcash')" />
-            <Button label="Maya" severity="success"
-                    @click="selectedPaymentMethod = 'maya'; PaymentRef.toggleDialog('maya')" />
+            <Button
+                label="GCash"
+                severity="info"
+                @click="
+                    selectedPaymentMethod = 'gcash';
+                    PaymentRef.toggleDialog('gcash');
+                "
+            />
+            <Button
+                label="Maya"
+                severity="success"
+                @click="
+                    selectedPaymentMethod = 'maya';
+                    PaymentRef.toggleDialog('maya');
+                "
+            />
         </div>
         <div class="flex flex-column gap-3 mt-5">
-            <p class="m-0">When using Cash, you are givin 15 minute time limit to arrive at the slot. The slot will be
-                open after 15 minutes.</p>
-            <Button label="Cash On Hand" @click="selectedPaymentMethod = 'cod'; PaymentRef.toggleDialog('cod')" />
+            <p class="m-0">
+                When using Cash, you are givin 15 minute time limit to arrive at the slot. The slot will be open after
+                15 minutes.
+            </p>
+            <Button
+                label="Cash On Hand"
+                @click="
+                    selectedPaymentMethod = 'cod';
+                    PaymentRef.toggleDialog('cod');
+                "
+            />
         </div>
     </Dialog>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
